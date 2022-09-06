348d65099e93f6f55af6b25c8e536477b0a7a478
shrey <shrey100@gmail.com>
2022-09-06 17:48:09 +0530

    E-commerce added

---
+++ E-commerce_clone/README.md
@@ -0,0 +1,7 @@
+# About
+
+- An E-commerce clone built with HTML/CSS and JavaScript using DOM manipulation.
+
+# Hosted link
+
+https://shraykumar100.github.io/MERN_ClassTest/06-03-2022/
---
+++ E-commerce_clone/index.js
@@ -0,0 +1,40 @@
+function trigger1() {
+    var spec = document.getElementById
+        ("specs1");
+
+    if (spec.classList[0] == "none") {
+        spec.classList.remove("none");
+    } else {
+        spec.classList.add("none");
+    }
+}
+function trigger2() {
+    var spec = document.getElementById
+        ("specs2");
+
+    if (spec.classList[0] == "none") {
+        spec.classList.remove("none");
+    } else {
+        spec.classList.add("none");
+    }
+}
+function trigger3() {
+    var spec = document.getElementById
+        ("specs3");
+
+    if (spec.classList[0] == "none") {
+        spec.classList.remove("none");
+    } else {
+        spec.classList.add("none");
+    }
+}
+function trigger4() {
+    var spec = document.getElementById
+        ("specs4");
+
+    if (spec.classList[0] == "none") {
+        spec.classList.remove("none");
+    } else {
+        spec.classList.add("none");
+    }
+}
